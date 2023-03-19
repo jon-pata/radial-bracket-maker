e6febf3df9382fe60ee2438cf2b7b6601487fca7
Jonathan Pata <jon.pata@gmail.com>
2023-03-19 13:54:56 -0400

    Setup Component Structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 import './App.css'
+import { Cell } from './components/Radial/Ring/Match/Cell/Cell'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+      <label for="customRange3" class="form-label">Example range</label>
+      <input type="range" class="form-range" min="1" max="8" step="1" id="customRange3"></input>
       <p>Hello world!</p>
+      <Cell></Cell>
+      <Cell></Cell>
     </div>
   )
 }
---
+++ src/components/Radial/Ring/Match/Cell/Cell.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import data from '../../../../../assets/tournementData_start.json'
+
+export const Cell = () => {
+    
+    console.log(data);
+
+    return (
+        <>
+            <h1>team</h1>
+        </>
+    )
+}
