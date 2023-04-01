de038d886937ade1c700a5a7463841152710e493
Courtney Pettiford <courtneypettiford@Courtneys-MacBook-Pro.local>
2023-04-01 11:21:43 -0400

    fix display of teams

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,24 @@ function App() {
     setInitialData(e.target.value);
   }
 
+  const generateTeams = (round) => {
+    const teams = [];
+    for(let i = 2**(round -1); i > 0; i--){
+      teams.push({
+        color: "#333333"
+      })
+    }
+    return teams;
+  }
+
   const setInitialData = (numRounds) => {
     let d = {};
     d.rounds = [];
-    for(let i = 0; i < numRounds; i++){
-      d.rounds.push({"roundOf": 2**(numRounds-1),
-                    "teams": []});
+    for(let i = numRounds; i > 0; i--){
+      d.rounds.push({
+        "roundOf": 2**(i),
+        "teams": generateTeams(i)
+      });
     }
     setData(d);
     console.log(`data: ${data}`);
@@ -25,7 +37,7 @@ function App() {
   return (
     <div className="App">
       <label htmlFor="customRange3" className="form-label">Example range</label>
-      <input type="range" className="form-range" min="1" max="8"
+      <input type="range" className="form-range" min="1" max="8" 
         step="1" id="customRange3" onChange={handleSliderChange} ></input>
       <Bracket data={data} setData={setData} roundCount={roundCount} />
     </div>
